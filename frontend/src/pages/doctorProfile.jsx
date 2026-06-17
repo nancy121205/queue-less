db56aab48f70fbe6d6f277ff8c73d0b726be50a6
Nancy <nancyverma.121205@gmail.com>
2026-06-17 19:27:03 +0530

    feat: book appointments for patients and display slot details

--- frontend/src/pages/doctorProfile.jsx
+++ frontend/src/pages/doctorProfile.jsx
@@ -22,6 +22,23 @@ function DoctorProfile(){
             .then(res => setAppointments(res.data))
             .catch(() => setError("Failed to load appointments"))
     }, [doctorId])
+
+    async function handleBook(availability_id) {
+        try{
+            setError("")
+            setSuccess("")
+            const response = await axios.post("http://localhost:8000/appointments", 
+                {doctor_id: doctorId, availability_id : availability_id},
+                {headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }}
+            );
+            setSuccess(response.data.message + ` Start time: ${response.data.start_time}`)
+            setError("")
+        }
+        catch(err){
+            setSuccess("")
+            setError(err.response?.data?.detail || "Error in booking appointment"); 
+        }
+    }
     return(
         <div>
             <h1>Doctor Profile</h1>
@@ -36,18 +53,18 @@ function DoctorProfile(){
                     <p>Specialization: {info.specialization}</p>
                     <p>Hospital: {info.hospital_name}</p>
                     <p>Consult time: {info.avg_consult_mins} mins</p>
-                    <p>Fees: {info.name}</p>
+                    <p>Fees: {info.fees}</p>
                 </div>
 
                 <h2>Slots availabile:</h2>
-                {appointments.map((slot) => (
+                {appointments.map((appointment) => (
                     <div>
-                        <p>Date: {new Date(slot.date).toLocaleDateString()}</p>
-                        <p>Start: {new Date(slot.start_time).toLocaleTimeString()}</p>
-                        <p>End: {new Date(slot.end_time).toLocaleTimeString()}</p>
-                        <p>Spots left: {slot.max_patients - slot.booked_patients}</p>
+                        <p>Date: {new Date(appointment.date).toLocaleDateString()}</p>
+                        <p>Start: {new Date(appointment.start_time).toLocaleTimeString()}</p>
+                        <p>End: {new Date(appointment.end_time).toLocaleTimeString()}</p>
+                        <p>Spots left: {appointment.max_patients - appointment.booked_patients}</p>
 
-                        <button onClick={() => handleBook(slot.id)}>
+                        <button onClick={() => handleBook(appointment.id)}>
                             Book slot
                         </button>
                     </div>
